7859a2cbbe6047c995c348df48438a088a3ace4a
Charlie Fredberg <charliefredberg@outlook.com>
2026-06-29 22:17:04 -0700

    made failing cameras come back

--- camera_publisher/usb_publisher.py
+++ camera_publisher/usb_publisher.py
@@ -26,6 +26,47 @@ class CameraStoppedReadingError(Exception):
     def __str__(self):
         return "The camera has unexpectedly stopped returning frames"
 
+class Camera():
+    def __init__(self, flip: bool, gst_str: str, name="camera": str):
+        self.name = name
+        self.flip = flip
+        self.cap_reopen_timer = None
+        self.gst_str = gst_str
+        
+        print(f"{self.name} gst_str: {self.gst_str}")
+
+        self.open_cap()
+
+    def open_cap(self):
+        self.cap_reopen_timer = time.time()
+        self.cap = cv2.VideoCapture(self.gst_str, cv2.CAP_GSTREAMER)
+        print(f"{self.name} Opening...")
+
+    def read_frame(self):
+        if self.cap_reopen_timer != None:
+            diff = time.time() - self.cap_reopen_timer
+            if diff > 2:
+                self.cap_reopen_timer = None
+        elif self.cap.isOpened():
+            self.cap_reopen_timer = None
+            ret, frame = self.cap.read()
+            if ret:
+                if self.flip:
+                    frame = cv2.rotate(frame, cv2.ROTATE_180)
+                return frame
+            else:
+                frame = np.zeros((240, 320, 3), dtype=np.uint8)
+                frame = cv2.putText(frame, 'No Signal', (300,240), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+                return frame
+        else:
+            self.open_cap()
+        
+        print(f"{self.name} not open")
+
+        frame = np.zeros((240, 320, 3), dtype=np.uint8)
+        frame = cv2.putText(frame, f'{self.name} capture not open', (0,240), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+        return frame
+
 class UsbCameraPublisher(Node):
 
     def __init__(self):
@@ -42,6 +83,7 @@ class UsbCameraPublisher(Node):
         self.declare_parameter('ir', False)
         self.declare_parameter('pi', False)
         self.declare_parameter('flip', False)
+        self.declare_parameter('legion', False)
         use_name = self.get_parameter('use_name').get_parameter_value().bool_value
         camera_name = self.get_parameter('camera_name').get_parameter_value().string_value
         port = self.get_parameter('port').get_parameter_value().string_value
@@ -53,7 +95,8 @@ class UsbCameraPublisher(Node):
         using_small = self.get_parameter('small').get_parameter_value().bool_value
         using_ir = self.get_parameter('ir').get_parameter_value().bool_value
         using_pi = self.get_parameter('pi').get_parameter_value().bool_value
-        self.flip = self.get_parameter('flip').get_parameter_value().bool_value
+        flip = self.get_parameter('flip').get_parameter_value().bool_value
+        legion = self.get_parameter('legion').get_parameter_value().bool_value
 
         qos = QoSProfile(
             depth=1,
@@ -92,9 +135,12 @@ class UsbCameraPublisher(Node):
         elif using_ir:
             print("Using IR Camera Stream")
             if use_name: 
-                gst_str = f'v4l2src device="/dev/v4l/{type_cam_src}/{camera_name}" ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 ! appsink'
+                gst_str = f'v4l2src device="/dev/v4l/{type_cam_src}/{camera_name}" ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! appsink'
             else:
-                gst_str = f'v4l2src device="/dev/v4l/by-path/platform-3610000.usb-usb-0:{port}:1.0-video-index0" extra-controls="c,num_video_buffers=2" ! videoscale ! video/x-raw,width=320,height=240 ! appsink'
+                if not legion:
+                    gst_str = f'v4l2src device="/dev/v4l/by-path/platform-3610000.usb-usb-0:{port}:1.0-video-index0" extra-controls="c,num_video_buffers=2" ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! appsink'
+                else:
+                    gst_str = f'v4l2src device="/dev/v4l/by-path/pci-0000:80:14.0-usb-0:{port}:1.0-video-index0" ! videoscale ! video/x-raw,width=160,height=120,framerate=9/1,format=UYVY ! videoconvert ! video/x-raw,format=BGR ! appsink'
         elif using_pi:
             gst_str = f'nvarguscamerasrc sensor-id={port} ! video/x-raw(memory:NVMM),width=320,height=240,framerate=15/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink'
             #  max-buffers=1 drop=true sync=false
@@ -107,24 +153,28 @@ class UsbCameraPublisher(Node):
 
         print(f"gst_str: {gst_str}")
 
-        self.cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
-        print("Camera Opening...")
-        sleep(2)
-        if not self.cap.isOpened():
-            raise CameraNotOpenError
-        print("Camera Open!")
+        # self.cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+        # print("Camera Opening...")
+        # sleep(3)
+        # if not self.cap.isOpened():
+        #     raise CameraNotOpenError
+        # print("Camera Open!")
+
+        self.cam = Camera(flip, gst_str)
 
         timer_period = 1/30  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            raise CameraStoppedReadingError
+        # ret, frame = self.cap.read()
+        # if not ret:
+        #     raise CameraStoppedReadingError
 
-        if self.flip:
-            frame = cv2.rotate(frame, cv2.ROTATE_180)
+        # if self.flip:
+        #     frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+        frame = cam.read_frame()
         
         msg = self.bridge.cv2_to_compressed_imgmsg(frame, dst_format='jpg')
 
@@ -141,18 +191,27 @@ def main(args=None):
 
     usb_camera_publisher = UsbCameraPublisher()
 
-    try:
-        rclpy.spin(usb_camera_publisher)
-    except Exception as e:
-        print(e)
-        if isinstance(e, CameraStoppedReadingError):
-            usb_camera_publisher.cap.release()
-    finally:
-        # Destroy the node explicitly
-        # (optional - otherwise it will be done automatically
-        # when the garbage collector destroys the node object)
-        usb_camera_publisher.destroy_node()
-        rclpy.shutdown()
+    while True:
+        try:
+            rclpy.spin(unified_publisher)
+        except Exception as e:
+            print("Something went wrong.  Restarting camera feed now.")
+            unified_publisher = UnifiedPublisher()
+
+    # try:
+    #     rclpy.spin(usb_camera_publisher)
+    # except Exception as e:
+    #     print(e)
+    #     if isinstance(e, CameraStoppedReadingError):
+    #         usb_camera_publisher.cap.release()
+    # finally:
+    #     # Destroy the node explicitly
+    #     # (optional - otherwise it will be done automatically
+    #     # when the garbage collector destroys the node object)
+    #     usb_camera_publisher.destroy_node()
+    #     rclpy.shutdown()
+    usb_camera_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
